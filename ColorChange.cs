0d06befc16b0d5d083dd12d9a4f608dd746095db
Brandon Nguyen <lrbn86@gmail.com>
2018-12-30 21:21:44 -0500

    Add files via upload

---
+++ ColorChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+	float timeLeft;
+	Color targetColor;
+
+	Color [] colors = {Color.grey, Color.magenta, Color.black};
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeLeft <= Time.deltaTime) {
+			GetComponent<Renderer>().material.color = targetColor;
+			targetColor = colors[Random.Range(0,2)];
+			timeLeft = 1.0f;
+		} else {
+			GetComponent<Renderer>().material.color = Color.Lerp(GetComponent<Renderer>().material.color, targetColor, Time.deltaTime / timeLeft);
+			timeLeft -= Time.deltaTime;
+		}
+	}
+}
---
+++ ConveyorBelt.cs
@@ -0,0 +1,21 @@
+﻿     using UnityEngine;
+     
+     public class ConveyorBelt : MonoBehaviour {
+     
+         public float speed = 20f;
+         bool on = true;
+         Vector2 offset = new Vector2(0f,0f);
+         
+     
+      
+     
+         void OnCollisionStay(Collision obj ){
+             float beltVelocity = speed * Time.deltaTime;
+             GetComponent<Rigidbody>().velocity = beltVelocity * transform.forward; 
+         }
+     
+         void Update(){
+             offset += new Vector2(0,0.1f) * Time.deltaTime;
+             GetComponent<Renderer>().material.SetTextureOffset ("_MainTex", offset);
+         }
+     }
